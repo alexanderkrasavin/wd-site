e0c4dfe572c6b97b421aba7eb18f133638d696a5
unknown <itkrasavin@gmail.com>
2023-09-13 18:53:26 +0300

    updated header function

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,14 @@
 const burgerButton = document.querySelector('.burger__item');
+const header = document.querySelector('.header');
+const firstLine = document.querySelector('.first-line');
+const secondLine = document.querySelector('.second-line');
+const thirdLine = document.querySelector('.third-line');
+const navigation = document.querySelector('.nav__list');
+
 burgerButton.addEventListener('click', () => {
-  alert('hi')
-})
+  header.classList.toggle('header_active');
+  firstLine.classList.toggle('first-line_active');
+  secondLine.classList.toggle('second-line_active');
+  thirdLine.classList.toggle('third-line_active');
+  navigation.classList.toggle('nav__list_active');
+});
