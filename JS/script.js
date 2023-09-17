37e250c883c29c3ce2d22aaab3d998ca42544053
unknown <itkrasavin@gmail.com>
2023-09-17 16:22:00 +0300

    implemented header function

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,12 @@
 const burgerButton = document.querySelector('.burger__item');
+const burgerLine = document.querySelector('.burger__line');
 const header = document.querySelector('.header');
-const firstLine = document.querySelector('.first-line');
-const secondLine = document.querySelector('.second-line');
-const thirdLine = document.querySelector('.third-line');
 const navigation = document.querySelector('.nav__list');
+const body = document.querySelector('body');
 
 burgerButton.addEventListener('click', () => {
-  header.classList.toggle('header_active');
-  firstLine.classList.toggle('first-line_active');
-  secondLine.classList.toggle('second-line_active');
-  thirdLine.classList.toggle('third-line_active');
-  navigation.classList.toggle('nav__list_active');
+  burgerButton.classList.toggle('active')
+  burgerLine.classList.toggle('active')
+  navigation.classList.toggle('active');
+  body.classList.toggle('lock')
 });
